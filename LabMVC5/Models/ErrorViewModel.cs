5658ed3ab3aa39e395bbca731bac2661dfb74851
KaneHydra <kanehydra@gmail.com>
2024-07-22 16:08:51 +0800

    加入專案檔案。

---
+++ LabMVC5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabMVC5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
